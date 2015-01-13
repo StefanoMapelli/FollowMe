4623d1f2f747496060f0b8c679b9aa67b944cba4
maru <maru@maru-PC.homenet.telecomitalia.it>
2015-01-13 11:31:01 +0100

    Aggiunta classe statica ParseManager per gestire le comunicazioni col backend.

---
+++ src/com/example/followme/ParseManager.java
@@ -0,0 +1,76 @@
+package com.example.followme;
+
+import java.util.List;
+
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseObject;
+import com.parse.ParseQuery;
+
+public class ParseManager {
+	
+	/**
+	 * Metodo che aggiorna l'indirizzo ip salvato su Parse della tupla con numero di telefono
+	 * pari a phoneNumber.
+	 * @param phoneNumber : numero di telefono dell'utente di cui vogliamo modificare l'ip
+	 */
+	public static void updateIpAddress(String phoneNumber, String ipAddress)
+	{
+		final String ip = ipAddress;
+		final String pNumber = phoneNumber;
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("Address");
+		query.whereEqualTo("phone", phoneNumber);
+		query.findInBackground(new FindCallback<ParseObject>() 
+				{
+					@Override
+					public void done(List<ParseObject> objects,ParseException e) 
+					{
+						if(e == null)
+						{
+							if(!objects.isEmpty())
+							{
+								objects.get(0).put("ipAddress",ip);
+								objects.get(0).saveInBackground();
+							}
+							else
+							{
+								ParseObject address = new ParseObject("Address");
+								address.put("phone", pNumber);
+								address.put("ipAddress", ip);
+								address.saveInBackground();											
+							}
+						}
+					}
+				}
+		);
+	}
+	
+	/**
+	 * Metodo che cerca sulla tabella Address in Parse la tupla avente il dato numero di telefono
+	 * e ritorna il campo indirizzo ip.
+	 * @param phoneNumber : numero di telefono dell'utente di cui si vuole conoscere l'ip.
+	 * @return : il campo indirizzo ip della tupla identificata da numero di telefono pari a phoneNumber.
+	 */
+	public static String getIpAddress(String phoneNumber)
+	{
+		List<ParseObject> objects = null;
+		ParseQuery<ParseObject> query = ParseQuery.getQuery("Address");
+		query.whereEqualTo("phone", phoneNumber);
+		try {
+			objects = query.find();
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		if(!objects.isEmpty())
+		{
+			return objects.get(0).getString("ipAddress");
+		}
+		else
+		{
+			return null;
+		}
+	}
+
+}
