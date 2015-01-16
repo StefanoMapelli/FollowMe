250d059a0183af829e3e07f38f91c84317bcb436
StefanoMapelli <stefano5.mapelli@mail.polimi.it>
2015-01-16 15:01:42 +0100

    Altra roba

---
+++ src/com/example/followme/ChooseContactActivity.java
@@ -0,0 +1,15 @@
+package com.example.followme;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class ChooseContactActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_choose_contact);
+	}
+}
