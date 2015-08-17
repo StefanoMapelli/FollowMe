4ac9ace12b654144f16066de16bf51cbda018690
StefanoMapelli <stefano5.mapelli@mail.polimi.it>
2015-08-17 17:33:20 +0200

    Layout lista richieste

--- src/com/example/followme/RequestsListActivity.java
+++ src/com/example/followme/RequestsListActivity.java
@@ -5,18 +5,18 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class ShareActivity extends ActionBarActivity {
+public class RequestsListActivity extends ActionBarActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.share_layout);
+		setContentView(R.layout.requests_list_layout);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.share, menu);
+		getMenuInflater().inflate(R.menu.requests_list, menu);
 		return true;
 	}
 
