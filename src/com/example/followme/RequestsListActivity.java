46391f232e16e394a34c8e8fe43eb0d32fc5bb41
StefanoMapelli <stefano5.mapelli@mail.polimi.it>
2015-08-18 15:51:34 +0200

    RequestCustom adapter

--- src/com/example/followme/RequestsListActivity.java
+++ src/com/example/followme/RequestsListActivity.java
@@ -1,16 +1,49 @@
 package com.example.followme;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
 
 public class RequestsListActivity extends ActionBarActivity {
+	
+	private ListView listViewRequests;
+	private Request[] requestsItems;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.requests_list_layout);
+		
+		//requestsList listView setting 
+		
+		//get listview
+		listViewRequests = (ListView) this.findViewById(R.id.requestsList);
+	
+		ArrayList<Request> requests = new ArrayList<Request>();
+		
+		Iterator<Request> iter = requests.iterator();
+		Request r;
+		int j=0;
+		requestsItems = new Request[requests.size()];
+		
+		while(iter.hasNext())
+		{
+			r = iter.next();
+			
+			requestsItems[j] = r;
+			j++;
+		}
+		
+		//set the adapter
+		RequestCustomAdapter adapter = new RequestCustomAdapter(this, requestsItems);
+		listViewRequests.setAdapter(adapter);
+		
 	}
 
 	@Override
@@ -31,4 +64,8 @@ public class RequestsListActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
+	
+	
+	
+	
 }
