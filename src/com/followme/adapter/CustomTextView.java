cd9c27317630bfdbccaddd5796fa72178a09a8b4
maru <maru@maru-PC.homenet.telecomitalia.it>
2015-09-12 11:01:47 +0200

    sistemati font text view

---
+++ src/com/followme/adapter/CustomTextView.java
@@ -0,0 +1,31 @@
+package com.followme.adapter;
+
+import android.content.Context;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+public class CustomTextView extends TextView {
+
+    public CustomTextView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init();
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public CustomTextView(Context context) {
+        super(context);
+        init();
+    }
+
+    private void init() {
+        Typeface tf = Typeface.createFromAsset(getContext().getAssets(),
+                                               "fonts/FORTE.TTF");
+        setTypeface(tf);
+    }
+
+}
