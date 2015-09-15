30b7013d5b5b6e39710f19a1d7f6fb55ec96b280
maru <maru@maru-PC.homenet.telecomitalia.it>
2015-09-15 19:04:26 +0200

    follow activity

--- src/com/followme/activity/FollowActivity.java
+++ src/com/followme/activity/FollowActivity.java
@@ -1,18 +1,73 @@
 package com.followme.activity;
 
 import com.followme.activity.R;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.GoogleMap.InfoWindowAdapter;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class FollowActivity extends ActionBarSuperClassActivity{
-
+public class FollowActivity extends ActionBarSuperClassActivity
+{
+	private GoogleMap map;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.follow_activity_layout);
+		map = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.mapFollowActivity)).getMap();
+		
+		map.getUiSettings().setMapToolbarEnabled(false);
+		
+		//inserimento marker
+		map.addMarker(new MarkerOptions()
+		.position(new LatLng(0, 0))
+		.icon(BitmapDescriptorFactory.fromResource(R.drawable.fence))
+		.title("Fence"));
+		
+		map.addMarker(new MarkerOptions()
+		.position(new LatLng(0, 0))
+		.icon(BitmapDescriptorFactory.fromResource(R.drawable.destination))
+		.title("Destination"));
+		
+		map.addMarker(new MarkerOptions()
+		.position(new LatLng(0, 0))
+		.icon(BitmapDescriptorFactory.fromResource(R.drawable.follow))
+		.title("Path"));
+		
+		//info window setting
+		map.setInfoWindowAdapter(new InfoWindowAdapter() {
+			@Override
+			public View getInfoWindow(Marker marker) {
+				return null;
+			}
+
+			@Override
+			public View getInfoContents(Marker marker) 
+			{
+				if(marker.getTitle().compareTo("Fence")==0)
+				{
+					startFenceOnClickHandler(null);
+				}
+				else if(marker.getTitle().compareTo("Destination")==0)
+				{
+					startDestinationOnClickHandler(null);
+				}
+				else
+				{
+					startPathOnClickHandler(null);
+				}	
+				
+				return null;
+			}
+		});	
 	}
 
 	//onClickHandler quando viene selezionata la modalità fence
